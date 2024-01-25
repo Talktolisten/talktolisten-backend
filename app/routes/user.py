94a0e01bf6956fd52184cfd0f6e0195e38d371aa
hieuminh65 <hieuminhnguyen@usf.edu>
2024-01-25 15:27:01 -0500

    Update user endpoints

--- app/routes/user.py
+++ app/routes/user.py
@@ -6,7 +6,7 @@ from sqlalchemy import func
 from .. import models
 from app.schemas import user
 from ..database import get_db
-
+import app.utils as utils
 
 router = APIRouter(
     prefix="/user",
@@ -18,9 +18,25 @@ router = APIRouter(
 def create_user(
     user: user.UserCreate,
     db: Session = Depends(get_db), 
-):
+):  
     new_user = models.User(**user.dict())
+
+    new_user.dob = utils.format_dob(user.dob)
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@router.get("/{id}", response_model=user.UserGet)
+def get_user(
+    id: str,
+    db: Session = Depends(get_db), 
+):
+    user = db.query(models.User).filter(models.User.user_id == id).first()
+    user.dob = utils.format_dob_str(user.dob)
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    
+    return user
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -10,6 +10,23 @@ class UserCreate(BaseModel):
     gmail: str
     first_name: str
     last_name: str
+    dob: Optional[str]
+
+    class Config:
+        from_attributes = True
+
+class UserGet(BaseModel):
+    user_id: str
+    username: str
+    gmail: str
+    first_name: str
+    last_name: str
+    dob: Optional[str]
+    subscription: str
+    bio: Optional[str]
+    profile_picture: Optional[str]
+    status: str
+    theme: str
 
     class Config:
         from_attributes = True
