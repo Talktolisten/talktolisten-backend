fbb4f05fd838768cc78d9ec6ea99804feb4764fa
hieuminh65 <hieuminhnguyen@usf.edu>
2024-04-22 14:44:59 -0400

    update voice schemas

--- app/schemas/voice.py
+++ app/schemas/voice.py
@@ -8,6 +8,9 @@ class VoiceGet(BaseModel):
     voice_id: int
     voice_name: str
     voice_description: Optional[str]
+    gender: Optional[str]
+    language: Optional[str]
+    style: Optional[str]
     sample_url: str
 
     class Config:
@@ -16,6 +19,9 @@ class VoiceGet(BaseModel):
 class VoiceCreate(BaseModel):
     voice_name: str
     voice_description: str
+    gender: str
+    language: Optional[str]
+    style: Optional[str]
     created_by: str
 
     class Config:
@@ -24,6 +30,7 @@ class VoiceCreate(BaseModel):
 class VoiceUpdate(BaseModel):
     voice_name: Optional[str]
     voice_description: Optional[str]
+    style: Optional[str]
 
     class Config:
         from_attributes = True
